6f85b26f689feefe126752fc3d71b8c69670dba5
Cr1msonEdge <91876824+Cr1msonEdge@users.noreply.github.com>
2025-04-13 20:27:50 +0300

    fix: distribution fix

--- helper/utilities.py
+++ helper/utilities.py
@@ -13,25 +13,25 @@ def calculate_mask_percentage(mask):
     return ratio
 
 
-def get_bin(mask, max_bins=5, top_bin_percentage=15):
+def get_bin(mask, max_bins=5, top_bin_percentage=10):
     """
     Return the number of bin for a mask
     
-    Params:
+    Args:
     max_bins: the number of bins used for division
     top_bin_percentage: the masks that has percentage more than this parameters will have the bin number 'max_bins - 1'
     """
-    mask = mask.squeeze()
-    mask = np.where(mask == 1, 1, 0)
-    ratio = np.sum(mask, axis=(0, 1)) / (mask.shape[0] * mask.shape[1]) * 100
+    mask = np.squeeze(mask)
+    mask = np.where(mask == 1, 1, 0)  # убедимся, что бинарная
+    ratio = np.sum(mask) / mask.size * 100
+
     if ratio == 0:
         return 0
     
-    result = int(ratio)
     step = top_bin_percentage // (max_bins - 1) + 1
     
-    for i in range(1, max_bins):
-        if result in range(step * (i - 1), step * i):
+    for i in range(1, max_bins - 1):
+        if ratio < i * step:
             return i
     
     return max_bins - 1
@@ -58,13 +58,15 @@ def save_train_test_split(dataset_name: str, images, masks, val_size=0.1, test_s
             os.makedirs(d)
     
     temp_ratio = val_size + test_size
+    mask_bins = [get_bin(mask) for mask in masks]
     train_images, temp_images, train_masks, temp_masks = train_test_split(
-        images, masks, test_size=temp_ratio, random_state=random_state
+        images, masks, test_size=temp_ratio, random_state=random_state, stratify=mask_bins
     )
     
+    mask_bins = [get_bin(mask) for mask in temp_masks]
     rel_test_ratio = test_size / temp_ratio if temp_ratio != 0 else 0
     val_images, test_images, val_masks, test_masks = train_test_split(
-        temp_images, temp_masks, test_size=rel_test_ratio, random_state=random_state
+        temp_images, temp_masks, test_size=rel_test_ratio, random_state=random_state, stratify=mask_bins
     )
     
     np.save(os.path.join(train_dir, 'images.npy'), train_images)
